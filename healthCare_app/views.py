40a4f8c36c8ba84f3da4acd4443bc12b7b0fcf2e
Tripti Pal Kaur <triptipalkaur@gmail.com>
2022-11-30 13:45:37 +0800

    yi kai

--- healthCare_app/views.py
+++ healthCare_app/views.py
@@ -21,7 +21,7 @@ firebaseConfig = {
     'messagingSenderId': "229559686647",
     'appId': "1:229559686647:web:a93fd4e466a9f2adb80af2",
     'measurementId': "G-WM3FSPBPDV",
-    'serviceAccount': "C:\\Users\\ben_l\\OneDrive\\Desktop\\Uni Stuff\\Web Development\\health-care-web-212e0-firebase-adminsdk-nyf5g-9ba2dd4249.json"
+    'serviceAccount': "C:\\Users\\tript\\Desktop\\University Year 2 Sem 6\\Semester 6 Sept 2022\\Internet & Web Development\\healthCare_Web-master\\healthCare_Web-master\\health-care-web-212e0-firebase-adminsdk-nyf5g-9ba2dd4249.json"
 }
 
 # Initialize Firebase
